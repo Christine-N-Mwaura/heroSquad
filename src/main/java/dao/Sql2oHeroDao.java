6df166494b242d0387d21db4a2a9d3dcdda98dc3
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-07 20:37:01 +0300

    create a dao file.

--- src/main/java/dao/Sql2oHeroDao.java
+++ src/main/java/dao/Sql2oHeroDao.java
@@ -1,4 +1,51 @@
 package dao;
 
-public class Sql2oHeroDao {
+import hero.Individual_Hero;
+import org.sql2o.*;
+import java.util.List;
+
+;
+public class Sql2oHeroDao implements heroDao{
+
+    private final Sql2o sql2o;
+    public Sql2oHeroDao(Sql2o sql2o){
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public List<Individual_Hero> getAll() {
+        return null;
+    }
+
+    @Override
+    public void add(Individual_Hero hero) {
+
+    }
+
+    @Override
+    public Individual_Hero findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(int id, String name, int Age) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAllHeroes() {
+
+    }
+//    @Override
+//    public void add(Individual_Hero hero){
+//      String sql = "INSERT INTO heroes (name) VALUES (:name)";
+//    }
+
+
+
 }
