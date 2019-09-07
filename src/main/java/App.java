a01c35b90d67ec3a56437f3fbd39be7a946e4a71
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-07 18:08:36 +0300

    add routes to navigate through the templaates.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,37 @@
+import hero.*;
+import static  spark.Spark.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 public class App {
+
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+
+        get("/",(req,res)->{
+            Map<String,Object> model = new HashMap<>();
+            return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+
+        get("/heroes/new",(req,res)->{
+            Map<String,Object> model = new HashMap<>();
+            return new ModelAndView(model,"post-heroForm.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/heroes/new", (req,res)->{
+            Map<String,Object> model = new HashMap<>();
+            String name = req.queryParams("name");
+            int Age = Integer.parseInt(req.queryParams("age"));
+            Individual_Hero hero = new Individual_Hero(name,Age);
+            return new ModelAndView(model,"success.hbs");
+
+        },new HandlebarsTemplateEngine());
+    }
 }
