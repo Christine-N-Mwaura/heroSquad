4b97331dd391fcc1eb985fdd1700eb7c40b860ef
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-07 20:40:33 +0300

    checks if an id is set to a hero once the hero is added.

--- src/test/java/dao/Sql2oHeroDaoTest.java
+++ src/test/java/dao/Sql2oHeroDaoTest.java
@@ -1,17 +1,38 @@
 package dao;
 
+import hero.Individual_Hero;
 import org.junit.After;
 import org.junit.Before;
-
+import org.junit.Test;
+import org.sql2o.*;
 import static org.junit.Assert.*;
 
+import java.sql.Connection;
+
+
 public class Sql2oHeroDaoTest {
+    private Sql2oHeroDao heroDao;
+    private Connection con;
 
     @Before
     public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString,"","");
+        heroDao = new Sql2oHeroDao(sql2o);
+        con = (Connection) sql2o.open();
+
     }
 
     @After
     public void tearDown() throws Exception {
+        con.close();
+    }
+
+    @Test
+    public void addingHeroSetsId() throws Exception {
+        Individual_Hero hero = new Individual_Hero("Superman",12);
+        heroDao.add(hero);
+        Individual_Hero foundHero = heroDao.findById(hero.getid());
+        assertEquals(hero, foundHero);
     }
 }
