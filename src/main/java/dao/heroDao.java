fd3def6fa9bf2935a29222352b3648296774fc59
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-07 20:38:08 +0300

    add an interface for saving a hero in the database.

--- src/main/java/dao/heroDao.java
+++ src/main/java/dao/heroDao.java
@@ -1,4 +1,25 @@
 package dao;
 
-public class heroDao {
+import hero.Individual_Hero;
+import java.util.List;
+
+public interface heroDao {
+    //list
+    List<Individual_Hero>getAll();
+
+    //CREATE
+    void add(Individual_Hero hero);
+
+    //READ
+    Individual_Hero findById(int id);
+
+    //UPDATE
+    void update(int id, String name,int Age);
+
+    //DELETE
+    void deleteById(int id);
+
+    //DELETE ALL HEROES
+    void clearAllHeroes();
+
 }
